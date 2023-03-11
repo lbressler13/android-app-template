a3c85ba848245157866d7ba7e68fd8d2f4aff26a
lbressler13 <lbressler13@gmail.com>
2023-03-11 17:25:16 -0500

    refactor: use base fragment

--- app/src/main/kotlin/xyz/lbres/androidapptemplate/ui/FirstFragment.kt
+++ app/src/main/kotlin/xyz/lbres/androidapptemplate/ui/FirstFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
-import xyz.lbres.androidapptemplate.BaseActivity
 import xyz.lbres.androidapptemplate.R
 import xyz.lbres.androidapptemplate.databinding.FragmentFirstBinding
 
 /**
  * Initial fragment in the app
  */
-class FirstFragment : NavHostFragment() {
+class FirstFragment : BaseFragment() {
     private lateinit var binding: FragmentFirstBinding
 
     /**
@@ -21,7 +19,7 @@ class FirstFragment : NavHostFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentFirstBinding.inflate(layoutInflater, container, false)
         binding.navigateText.setOnClickListener {
-            (requireActivity() as BaseActivity).runNavAction(R.id.navigateFirstToSecond)
+            requireBaseActivity().runNavAction(R.id.navigateFirstToSecond)
         }
 
         return binding.root
--- app/src/main/kotlin/xyz/lbres/androidapptemplate/ui/SecondFragment.kt
+++ app/src/main/kotlin/xyz/lbres/androidapptemplate/ui/SecondFragment.kt
@@ -4,24 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.NavHostFragment
-import xyz.lbres.androidapptemplate.BaseActivity
+import xyz.lbres.androidapptemplate.R
 import xyz.lbres.androidapptemplate.databinding.FragmentSecondBinding
 
 /**
  * Second fragment to navigate to
  */
-class SecondFragment : NavHostFragment() {
+class SecondFragment : BaseFragment() {
     private lateinit var binding: FragmentSecondBinding
+    override var actionBarTitleResId: Int = R.string.title_second_fragment
 
     /**
      * Initialize fragment
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSecondBinding.inflate(layoutInflater, container, false)
-        binding.popBackstackText.setOnClickListener {
-            (requireActivity() as BaseActivity).popBackStack()
-        }
+        binding.popBackstackText.setOnClickListener { closeFragment() }
 
         return binding.root
     }
